ee9eec597abee4c55098b627ef4f49d1771bfbf4
Szymon Małolepszy <szymon8999@wp.pl>
2020-05-04 19:56:33 +0200

    first packet tests

--- test.cpp
+++ test.cpp
@@ -42,6 +42,7 @@ TEST(HelloWorldTest, BasicMock) {
     MockDB mdb;
     MyDatabase db(mdb);
 
+    EXPECT_CALL(mdb, login("Terminator", "asdf"));
     ON_CALL(mdb, login("Terminator", "asdf")).WillByDefault(Return(1));
 
     // Act
@@ -49,6 +50,45 @@ TEST(HelloWorldTest, BasicMock) {
 
     // Assert
     EXPECT_EQ(retValue, 1);
+}
+
+
+TEST( PacketTest, CreatePacket )
+{
+    Packet::userDefault = "default";
+    
+    Packet p1(packet_t::ACK, "user");
+    Packet p2(packet_t::GAME, "");
+
+    EXPECT_EQ(p1.getType(), packet_t::ACK);
+    EXPECT_EQ(p1.getUser(), "user");
+
+    EXPECT_EQ(p2.getType(), packet_t::GAME);
+    EXPECT_EQ(p2.getUser(), "default");
+}
+
+TEST( PacketTest, SerializeAndDeserialize )
+{
+    size_t len = 512;
+    char bufferUsed[512];
+    
+    Packet p2(packet_t::GAME);
+    p2.serialize(bufferUsed, len);
+    Packet p3(bufferUsed, len);
+    
+    EXPECT_EQ(p2.getType(), packet_t::GAME);
+    EXPECT_EQ(p2.getType(), p3.getType());
+}
+
+TEST( PacketTest, CreateAckPacket )
+{
+    Packet::userDefault = "default";
+    int numberUsed = 10;
+    PacketAck p1(numberUsed, "");
+
+    EXPECT_EQ(p1.getType(), packet_t::ACK);
+    EXPECT_EQ(p1.getUser(), "default");
+
 }
  
 int main(int argc, char **argv) {
